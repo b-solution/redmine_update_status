e7d2ae22dd82b256186daf2d0317ff3794d7d87b
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-10-18 20:47:44 +0100

    commit

---
+++ init.rb
@@ -0,0 +1,14 @@
+Redmine::Plugin.register :redmine_update_status do
+  name 'Redmine Update Status plugin'
+  author 'BILEL KEDIDI'
+  description 'This is a plugin update status and %done when you log time'
+  version '0.0.1'
+  url 'http://github.com/bilel-kedidi'
+  author_url 'http://github.com/bilel-kedidi'
+
+end
+
+
+Rails.application.config.to_prepare do
+  require_dependency 'hooks'
+end
---
+++ lib/hooks.rb
@@ -0,0 +1,18 @@
+class Hooks < Redmine::Hook::ViewListener
+  render_on :view_timelog_edit_form_bottom, :partial=> 'hooks/redmine_app/show_issues_fields'
+  def controller_timelog_edit_before_save(context={ })
+    te = context[:time_entry]
+    params = context[:params]
+    issue = te.issue
+    done_ratio = params[:done_ratio]
+    status = params[:status_id]
+    issue.status_id = status
+    issue.done_ratio = done_ratio
+    issue.save
+  end
+
+  # def controller_issues_edit_after_save(context={})
+  #   te = context[:time_entry]
+  #   issue = context[:issue]
+  # end
+end
