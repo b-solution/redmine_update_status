eff8bb00b5360c5c762dfe93dd9453ebb5d60b94
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-10-23 06:46:39 +0100

    fix bug when issue not exist

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -4,11 +4,13 @@ class Hooks < Redmine::Hook::ViewListener
     te = context[:time_entry]
     params = context[:params]
     issue = te.issue
-    done_ratio = params[:done_ratio]
-    status = params[:status_id]
-    issue.status_id = status
-    issue.done_ratio = done_ratio
-    issue.save
+    if issue
+      done_ratio = params[:done_ratio]
+      status = params[:status_id]
+      issue.status_id = status
+      issue.done_ratio = done_ratio
+      issue.save
+    end
   end
 
   # def controller_issues_edit_after_save(context={})
